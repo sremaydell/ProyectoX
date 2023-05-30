997c6ca38a62e7f26e395fbbcc5ba64eeba0d258
Sremaydell Maria Grimote Javier <20220549@itla.edu.do>
2023-05-30 12:36:32 -0400

    #Inicio de Proyecto

---
+++ ProyectoX.Web/ProyectoX.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoX.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
